4367a2346ac76f21ceb873cd851719f0f609d629
rutvika09 <102011204+rutvika09@users.noreply.github.com>
2024-02-05 13:18:30 +0000

    varibales

---
+++ 01_basics/01var.js
@@ -0,0 +1,14 @@
+const accountId = 90702
+let accountEmail = "rutvika09@gmail.com"
+var accountPassword = "1234"
+accountCity = "Indore"
+
+//accountId = 2 //not allowed
+
+accountEmail = "ruhh@gmail.com"
+accountPassword = "09876"
+accountCity = "Bhopal"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
